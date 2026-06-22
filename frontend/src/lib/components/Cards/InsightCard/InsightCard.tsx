00b8e307ebe546ee5a6af064db0839875dc9bb60
Sam Pennington <sam@posthog.com>
2026-06-22 21:44:57 +0200

    fix: extract debounce constant and drop redundant JSDoc in InsightCard
    Promote the magic number 700 to DISPLAY_OPTIONS_PERSIST_DEBOUNCE_MS and
    remove a JSDoc comment that restates the prop name.
    
    Generated-By: PostHog Code
    Task-Id: 9f45f79f-1069-48ae-84ed-85fdf186cac4

--- frontend/src/lib/components/Cards/InsightCard/InsightCard.tsx
+++ frontend/src/lib/components/Cards/InsightCard/InsightCard.tsx
@@ -48,6 +48,7 @@ import { EditModeEdge, EditModeEdgeOverlay } from './EditModeEdgeOverlay'
 import { InsightMeta } from './InsightMeta'
 
 const IS_STORYBOOK = inStorybook() || inStorybookTestRunner()
+const DISPLAY_OPTIONS_PERSIST_DEBOUNCE_MS = 700
 
 export interface InsightCardProps extends Resizeable {
     /** Insight to display. */
@@ -187,7 +188,7 @@ function InsightCardInternal(
     const canPersistDisplayOptions = !!dashboardId && canEditInsight
     const persistDisplayOptions = useDebouncedCallback((node: Node) => {
         updateInsightDirect(insightRef.current, { query: node })
-    }, 700)
+    }, DISPLAY_OPTIONS_PERSIST_DEBOUNCE_MS)
 
     const insightLogicProps: InsightLogicProps = {
         dashboardItemId: insight.short_id,
